b30c13c2e3b5813c7483e3484ff510e6abdd7f5c
Rochelle Lewis <rlewis37@cnm.edu>
2017-02-13 09:47:35 -0700

    added background color css, added row 2

--- index.php
+++ index.php
@@ -40,8 +40,21 @@
 
 			<!-- row 1-->
 			<div class="row">
-				<div class="col-md-12">
+				<div class="col-xs-12">
 					<h1>Hai! I'm a Bootstrap Page! :D</h1>
+				</div>
+			</div>
+
+			<!-- row 2 -->
+			<div class="row">
+				<div class="col-md-4">
+					Column 4/12
+				</div>
+				<div class="col-md-4">
+					Column 4/12
+				</div>
+				<div class="col-md-4">
+					Column 4/12
 				</div>
 			</div>
 
